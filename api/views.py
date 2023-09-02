bec24f456d5d282b88de1f0d7719d40dc7ea5836
belemaaa <graceitamunoala@gmail.com>
2023-09-02 12:47:43 +0000

    pasword hashing and login fixes

--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password, check_password
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.authentication import authenticate
 from rest_framework.response import Response
+from django.contrib.auth.hashers import check_password
 from . import serializers
 from . import models
 
@@ -14,17 +16,19 @@ class Signup(APIView):
 
         if serializer.is_valid():
             username = serializer.validated_data.get('username')
-            password = serializer.validated_data.get('password')
+            raw_password = serializer.validated_data.get('password')
             email = serializer.validated_data.get('email')
             first_name = serializer.validated_data.get('first_name')
             last_name = serializer.validated_data.get('last_name')
 
+            hashed_password = make_password(raw_password)
             user_exists = models.Member.objects.filter(username=username)
             if user_exists:
                 return Response({'error': 'user with this username already exists.'}, status=status.HTTP_400_BAD_REQUEST)
-            serializer.save()
+            
+            serializer.save(password=hashed_password)
             return Response({'message': 'signup successful'}, status=status.HTTP_201_CREATED)
-        return Response({'errors: ', serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class Login(APIView):
     def post(self, request, *args, **kwargs):
@@ -33,9 +37,13 @@ class Login(APIView):
         if serializer.is_valid():
             username = serializer.validated_data.get('username')
             password = serializer.validated_data.get('password')
-            
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                return Response({'message': 'login successful'}, status=status.HTTP_200_OK)
+            try:
+                user = models.Member.objects.get(username=username)
+            except models.Member.DoesNotExist:
+                user = None
+
+            if user is not None and check_password(password, user.password):
+                return Response({'message': 'login successful'}, status=status.HTTP_200_OK)    
             return Response({'error': 'invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        
         return Response({'error': 'user does not exist.'}, status=status.HTTP_400_BAD_REQUEST)
